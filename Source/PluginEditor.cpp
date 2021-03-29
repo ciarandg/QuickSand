ea0651c4214353256144c99a9716e54c480a5c08
Ciaran <ciaran.deg@gmail.com>
2021-03-28 21:11:24 -0600

    Resize window, switch bg colour to custom one

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,7 @@
 QuickSandAudioProcessorEditor::QuickSandAudioProcessorEditor (QuickSandAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (880, 200);
 }
 
 QuickSandAudioProcessorEditor::~QuickSandAudioProcessorEditor()
@@ -25,16 +23,9 @@ QuickSandAudioProcessorEditor::~QuickSandAudioProcessorEditor()
 //==============================================================================
 void QuickSandAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (COLOUR_BG_DARK);
 }
 
 void QuickSandAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
 }
