41bf34f422e2d7fda5672087cae256c64eca910c
Ciaran <ciaran.deg@gmail.com>
2021-03-28 20:56:51 -0600

    Create labeled rotary slider class

---
+++ Source/GUI/LabeledRotary.h
@@ -0,0 +1,42 @@
+/*
+  ==============================================================================
+
+    LabeledRotary.h
+    Created: 28 Mar 2021 8:18:59pm
+    Author:  ciaran
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class LabeledRotary : public juce::Slider {
+public:
+  LabeledRotary(
+    double min_val, double max_val, double step, double initial,
+    std::string label_text, std::string suffix, juce::Colour highlight) {
+    // slider setup
+    setRange(min_val, max_val, step);
+    setValue(initial);
+    setTextValueSuffix(suffix);
+    // label setup
+    label.setText(label_text, juce::dontSendNotification);
+
+    // slider visuals
+    setSliderStyle(RotaryVerticalDrag);
+    setTextBoxStyle(TextBoxBelow, false, TEXT_BOX_WIDTH, TEXT_BOX_HEIGHT);
+    // label visuals
+    label.setFont({20.f, juce::Font::bold});
+    label.setJustificationType(juce::Justification::centred);
+    label.attachToComponent(this, false);
+    // colours
+    setColour(rotarySliderFillColourId,     highlight);
+    setColour(thumbColourId,                juce::Colours::transparentBlack);
+  };
+private:
+  static const int TEXT_BOX_WIDTH = 100;
+  static const int TEXT_BOX_HEIGHT = 25;
+  juce::Label label;
+};
