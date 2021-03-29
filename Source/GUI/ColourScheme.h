f2f095f3fc7366001a02db1bb1b14fc003c293e5
Ciaran <ciaran.deg@gmail.com>
2021-03-28 21:04:34 -0600

    Add colour scheme header

---
+++ Source/GUI/ColourScheme.h
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    ColourScheme.h
+    Created: 28 Mar 2021 9:02:02pm
+    Author:  ciaran
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+static const juce::Colour COLOUR_BG_DARK {0xff282828};
+static const juce::Colour COLOUR_BG_LIGHT {0xff3c3836};
+static const juce::Colour COLOUR_TEXT {0xfff2e5bc};
+
+static const juce::Colour COLOUR_ACCENT_1 {0xffA32164};
+static const juce::Colour COLOUR_ACCENT_2 {0xffF0DF60};
+static const juce::Colour COLOUR_ACCENT_3 {0xffF0489E};
+static const juce::Colour COLOUR_ACCENT_4 {0xff30DAF0};
+static const juce::Colour COLOUR_ACCENT_5 {0xff2995A3};
