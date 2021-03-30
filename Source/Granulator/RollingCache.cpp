27b00e2a9f380b3ab38ed02bbba9aa79c2d88ef5
Ciaran <ciaran.deg@gmail.com>
2021-03-30 00:19:46 -0600

    Fill out RollingCache class

--- Source/Granulator/RollingCache.cpp
+++ Source/Granulator/RollingCache.cpp
@@ -9,3 +9,54 @@
 */
 
 #include "RollingCache.h"
+#include <JuceHeader.h>
+
+RollingCache::RollingCache(uint capacity) : capacity{capacity} {
+  buffer.resize(capacity);
+};
+
+void RollingCache::write(float value) {
+  if (capacity == 0)
+    return;
+  buffer[head] = value;
+  inc_head();
+}
+
+std::vector<float> RollingCache::read_chunk(size_t chunk_size, int offset) {
+  if (!is_full())
+    DBG("CAN'T READ FROM UNFILLED BUFFER");
+  std::vector<float> out;
+  int start = wrap_index(head - offset - chunk_size);
+
+  int targetIndex = wrap_index(head - offset);
+  for (int i = start; i != targetIndex; i = wrap_index(i + 1)) {
+    out.push_back(buffer[i]);
+  }
+
+  return out;
+}
+
+void RollingCache::resize(uint new_size) {
+  buffer.resize(new_size);
+  for (int samp = 0; samp < new_size; ++samp) {
+    buffer[samp] = 0.f;
+  }
+  capacity = new_size;
+  head = 0;
+  filled = false;
+}
+
+size_t RollingCache::get_capacity() { return capacity; }
+bool RollingCache::is_full() { return filled; }
+
+int RollingCache::wrap_index(int i) {
+  while (i < 0)
+    i += capacity;
+  return i % capacity;
+}
+
+void RollingCache::inc_head() {
+  head = wrap_index(head + 1);
+  if (!filled && head == capacity - 1)
+    filled = true;
+}
--- Source/Granulator/RollingCache.h
+++ Source/Granulator/RollingCache.h
@@ -9,3 +9,26 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+#include <vector>
+
+class RollingCache {
+public:
+  RollingCache(uint capacity);
+
+  void write(float value);
+  std::vector<float> read_chunk(size_t chunk_size, int offset);
+  void resize(uint new_size);
+  size_t get_capacity();
+  bool is_full();
+
+private:
+  std::vector<float> buffer;
+  uint capacity = 0;    // capacity in samples
+  uint head = 0;        // incremented by inc_head()
+  bool filled = false;  // has the cache been filled yet?
+
+  int wrap_index(int i);
+  void inc_head();
+};
