af8c2877ff77e48f2aec1e1f99b66bf8e14dff98
Ciaran <ciaran.deg@gmail.com>
2021-03-30 00:35:54 -0600

    Fill out Granulator class

--- Source/Granulator/Granulator.cpp
+++ Source/Granulator/Granulator.cpp
@@ -9,3 +9,91 @@
 */
 
 #include "Granulator.h"
+
+Granulator::Granulator() : ringBuf(0){};
+
+std::vector<float> Granulator::read(int totalSamples) {
+  int samplesToFill = totalSamples;
+  int samplesFilled = 0;
+  std::vector<float> out;
+
+  if (!ringBuf.is_full()) {
+    for (; samplesFilled < samplesToFill; ++samplesFilled)
+      out.push_back(0.f);
+    return out;
+  }
+
+  std::vector<float> grain;
+  int maxOffset = (ringBuf.get_capacity() - grainSize) * randomness;
+  for (; samplesFilled < totalSamples; ++samplesFilled) {
+    float samp = 0;
+    if (oh.samplesToNextGrain-- <= 0) {
+      auto offset =
+          maxOffset <= 0
+              ? 0
+              : random.nextInt(
+                    maxOffset); // conditional prevents assertion failure
+      grain = ringBuf.read_chunk(grainSize, offset);
+      apply_ramp(grain);
+
+      oh.samplesToNextGrain = grainSize * overlap;
+
+      for (int i = 0; i < grain.size(); ++i) {
+        if (i < oh.data.size())
+          oh.data[i] += grain[i];
+        else
+          oh.data.push_back(grain[i]);
+      }
+    }
+
+    if (!oh.data.empty()) {
+      samp += oh.data.front();
+      oh.data.pop_front();
+    }
+    out.push_back(samp);
+  }
+
+  return out;
+}
+
+void Granulator::fill(juce::AudioBuffer<float> &source) {
+  std::vector<float> mono;
+  for (int samp = 0; samp < source.getNumSamples(); ++samp) {
+    mono.push_back(0);
+  }
+
+  for (int ch = 0; ch < source.getNumChannels(); ++ch) {
+    auto *lineIn = source.getReadPointer(ch);
+    for (int samp = 0; samp < source.getNumSamples(); ++samp) {
+      mono[samp] += lineIn[samp];
+    }
+  }
+
+  for (int samp = 0; samp < source.getNumSamples(); ++samp) {
+    ringBuf.write(mono[samp]);
+  }
+}
+
+void Granulator::resize(uint new_size) {
+  oh = {};
+  ringBuf.resize(new_size);
+}
+
+void Granulator::set_grain_size(float size) { grainSize = size; }
+
+void Granulator::set_randomness(float pct) { randomness = pct; }
+
+void Granulator::set_window(float pct) { window = pct; }
+
+void Granulator::set_overlap(float grains) { overlap = grains; }
+
+void Granulator::apply_ramp(std::vector<float> &dest) {
+  int size = dest.size();
+  int ramp_length = float(size) * window; // truncated
+  float inc = 1.0f / float(ramp_length);
+  for (int i = 0; i < ramp_length; ++i) {
+    float mult = inc * i;
+    dest[i] *= mult;
+    dest[size - 1 - i] *= mult;
+  }
+}
--- Source/Granulator/Granulator.h
+++ Source/Granulator/Granulator.h
@@ -9,3 +9,42 @@
 */
 
 #pragma once
+
+#include "RollingCache.h"
+#include <JuceHeader.h>
+
+class Granulator {
+public:
+  Granulator();
+
+  // PRE: size() >= numSamples
+  // poll sample from ringBuf numSamples times
+  // return vector containing random chunks
+  std::vector<float> read(int totalSamples);
+
+  // PRE: ringBuf has enough space to contain entirety of the requested channel
+  // fill ringBuf with the entirety of the specified channel of specified source
+  void fill(juce::AudioBuffer<float> &source);
+
+  void resize(uint new_size);
+  void set_grain_size(float size);
+  void set_randomness(float pct);
+  void set_window(float pct);
+  void set_overlap(float grains);
+
+private:
+  struct overhang {
+    std::deque<float> data;
+    int samplesToNextGrain = 0;
+  };
+
+  RollingCache ringBuf;
+  juce::Random random;
+  int grainSize;
+  float randomness;
+  float window;
+  float overlap;
+  struct overhang oh;
+  
+  void apply_ramp(std::vector<float> &dest);
+};
