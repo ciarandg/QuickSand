2372d722dd823753695f9e06c56c03a9f32110f4
Ciaran <ciaran.deg@gmail.com>
2021-04-03 21:01:09 -0600

    Make PluginEditor a Slider Listener

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -6,49 +6,59 @@
   ==============================================================================
 */
 
-#include "UI/LabeledRotary.h"
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
+#include "UI/LabeledRotary.h"
 
-const static int WINDOW_WIDTH  = 880;
+
+const static int WINDOW_WIDTH = 880;
 const static int WINDOW_HEIGHT = 200;
 
 //==============================================================================
-QuickSandAudioProcessorEditor::QuickSandAudioProcessorEditor (QuickSandAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    setSize (WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-    for (std::vector<LabeledRotary*> group : dial_groups) {
-      for (LabeledRotary* dial : group) addAndMakeVisible(*dial);
-    }
+QuickSandAudioProcessorEditor::QuickSandAudioProcessorEditor(
+    QuickSandAudioProcessor &p)
+    : AudioProcessorEditor(&p), audioProcessor(p) {
+  setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  
+  cacheSizeRotary.addListener(this);
+  grainSizeRotary.addListener(this);
+  grainShapeRotary.addListener(this);
+  grainRandomnessRotary.addListener(this);
+  grainOverlapRotary.addListener(this);
+  wetDryRotary.addListener(this);
+
+  for (std::vector<LabeledRotary *> group : dial_groups) {
+    for (LabeledRotary *dial : group)
+      addAndMakeVisible(*dial);
+  }
 }
 
-QuickSandAudioProcessorEditor::~QuickSandAudioProcessorEditor()
-{
-}
+QuickSandAudioProcessorEditor::~QuickSandAudioProcessorEditor() {}
 
 //==============================================================================
-void QuickSandAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    g.fillAll (COLOUR_BG_DARK);
-    g.setColour(COLOUR_BG_LIGHT);
-    for (int group = 1; group < dial_groups.size(); ++group) {
-      int line_x = getGroupX(group)-GROUP_PAD/2;
-      g.drawLine(line_x, 0, line_x, WINDOW_HEIGHT, GROUP_DIVIDER_WIDTH);
-    }
-    
-    g.setColour(COLOUR_ACCENT_1);
-    grain_shape_graphic.draw(grainShapeRotary.getValue()*0.01, g);
+void QuickSandAudioProcessorEditor::paint(juce::Graphics &g) {
+  g.fillAll(COLOUR_BG_DARK);
+  g.setColour(COLOUR_BG_LIGHT);
+  for (int group = 1; group < dial_groups.size(); ++group) {
+    int line_x = getGroupX(group) - GROUP_PAD / 2;
+    g.drawLine(line_x, 0, line_x, WINDOW_HEIGHT, GROUP_DIVIDER_WIDTH);
+  }
+
+  g.setColour(COLOUR_ACCENT_1);
+  grain_shape_graphic.draw(grainShapeRotary.getValue() * 0.01, g);
 }
 
-void QuickSandAudioProcessorEditor::resized()
-{
+void QuickSandAudioProcessorEditor::resized() {
   int dialCount = 0;
   for (int group = 0; group < dial_groups.size(); ++group) {
-    for (LabeledRotary* dial : dial_groups[group]) {
-      dial->setBounds(getDialX(dialCount), TOP_PAD, ROTARY_WIDTH, ROTARY_HEIGHT);
+    for (LabeledRotary *dial : dial_groups[group]) {
+      dial->setBounds(getDialX(dialCount), TOP_PAD, ROTARY_WIDTH,
+                      ROTARY_HEIGHT);
       ++dialCount;
     }
   }
+}
+
+void QuickSandAudioProcessorEditor::sliderValueChanged(juce::Slider *slider) {
+
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -8,86 +8,94 @@
 
 #pragma once
 
-#include <JuceHeader.h>
-#include <vector>
+#include "Data/ParamData.h"
 #include "PluginProcessor.h"
 #include "UI/ColourScheme.h"
-#include "UI/LabeledRotary.h"
 #include "UI/EnvelopeGraphic.h"
-#include "Data/ParamData.h"
+#include "UI/LabeledRotary.h"
+#include <JuceHeader.h>
+#include <vector>
 
 //==============================================================================
 /**
-*/
-class QuickSandAudioProcessorEditor  : public juce::AudioProcessorEditor
-{
+ */
+class QuickSandAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                      public juce::Slider::Listener {
 public:
-    QuickSandAudioProcessorEditor (QuickSandAudioProcessor&);
-    ~QuickSandAudioProcessorEditor() override;
+  QuickSandAudioProcessorEditor(QuickSandAudioProcessor &);
+  ~QuickSandAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+  //==============================================================================
+  void paint(juce::Graphics &) override;
+  void resized() override;
+  void sliderValueChanged(juce::Slider *slider) override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    QuickSandAudioProcessor& audioProcessor;
-    
-    static const int TOP_PAD = 40;
-    static const int ROTARY_WIDTH = 140;
-    static const int ROTARY_HEIGHT = 140;
-    static const int GROUP_PAD = 20;
-    static const int GROUP_DIVIDER_WIDTH = 6;
-    
-    LabeledRotary cacheSizeRotary { CACHE_SIZE_PARAM_DATA, COLOUR_ACCENT_2 };
-    juce::SliderParameterAttachment cacheSizeParamAttach { *audioProcessor.cacheSizeParameter, cacheSizeRotary };
-    LabeledRotary grainSizeRotary { GRAIN_SIZE_PARAM_DATA, COLOUR_ACCENT_1 };
-    juce::SliderParameterAttachment grainSizeParamAttach { *audioProcessor.grainSizeParameter, grainSizeRotary };
-    LabeledRotary grainShapeRotary { GRAIN_SHAPE_PARAM_DATA, COLOUR_ACCENT_1 };
-    juce::SliderParameterAttachment grainShapeParamAttach { *audioProcessor.grainShapeParameter, grainShapeRotary };
-    LabeledRotary grainRandomnessRotary { GRAIN_RAND_PARAM_DATA, COLOUR_ACCENT_1 };
-    juce::SliderParameterAttachment grainRandomnessParamAttach { *audioProcessor.randomnessParameter, grainRandomnessRotary };
-    LabeledRotary grainOverlapRotary { GRAIN_OVERLAP_PARAM_DATA, COLOUR_ACCENT_1 };
-    juce::SliderParameterAttachment grainOverlapParamAttach { *audioProcessor.grainOverlapParameter, grainOverlapRotary };
-    LabeledRotary wetDryRotary { WET_DRY_PARAM_DATA, COLOUR_ACCENT_5 };
-    juce::SliderParameterAttachment wetDryParamAttach { *audioProcessor.wetDryParameter, wetDryRotary };
-    
-    std::vector<std::vector<LabeledRotary*>> dial_groups = {
-      { &cacheSizeRotary },
-      { &grainSizeRotary, &grainShapeRotary, &grainRandomnessRotary, &grainOverlapRotary },
-      { &wetDryRotary }
-    };
-    
-    EnvelopeGraphic grain_shape_graphic{getDialX(2)+ROTARY_WIDTH/2, 75};
-    
-    int getDialX(int dial_index) {
-      std::vector<int> group_sizes;
-      for (std::vector<LabeledRotary*> group : dial_groups) {
-        group_sizes.push_back(group.size());
-      }
-      
-      int x_pos = 0, acc = 0;
-      for (int s : group_sizes) {
-        int group_end = acc+s;
-        
-        bool dial_in_group = dial_index < group_end;
-        x_pos += ROTARY_WIDTH*(dial_in_group ? dial_index-acc : s);
-        if (dial_in_group) break;
-        
-        x_pos += GROUP_PAD;
-        acc += s;
-      }
-      return x_pos;
-    }
-    
-    int getGroupX(int group_index) {
-      int dial_count = 0;
-      for (int g = 0; g < group_index; ++g) {
-        dial_count += dial_groups[g].size();
-      }
-      return getDialX(dial_count);
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  QuickSandAudioProcessor &audioProcessor;
+
+  static const int TOP_PAD = 40;
+  static const int ROTARY_WIDTH = 140;
+  static const int ROTARY_HEIGHT = 140;
+  static const int GROUP_PAD = 20;
+  static const int GROUP_DIVIDER_WIDTH = 6;
+
+  LabeledRotary cacheSizeRotary{CACHE_SIZE_PARAM_DATA, COLOUR_ACCENT_2};
+  juce::SliderParameterAttachment cacheSizeParamAttach{
+      *audioProcessor.cacheSizeParameter, cacheSizeRotary};
+  LabeledRotary grainSizeRotary{GRAIN_SIZE_PARAM_DATA, COLOUR_ACCENT_1};
+  juce::SliderParameterAttachment grainSizeParamAttach{
+      *audioProcessor.grainSizeParameter, grainSizeRotary};
+  LabeledRotary grainShapeRotary{GRAIN_SHAPE_PARAM_DATA, COLOUR_ACCENT_1};
+  juce::SliderParameterAttachment grainShapeParamAttach{
+      *audioProcessor.grainShapeParameter, grainShapeRotary};
+  LabeledRotary grainRandomnessRotary{GRAIN_RAND_PARAM_DATA, COLOUR_ACCENT_1};
+  juce::SliderParameterAttachment grainRandomnessParamAttach{
+      *audioProcessor.randomnessParameter, grainRandomnessRotary};
+  LabeledRotary grainOverlapRotary{GRAIN_OVERLAP_PARAM_DATA, COLOUR_ACCENT_1};
+  juce::SliderParameterAttachment grainOverlapParamAttach{
+      *audioProcessor.grainOverlapParameter, grainOverlapRotary};
+  LabeledRotary wetDryRotary{WET_DRY_PARAM_DATA, COLOUR_ACCENT_5};
+  juce::SliderParameterAttachment wetDryParamAttach{
+      *audioProcessor.wetDryParameter, wetDryRotary};
+
+  std::vector<std::vector<LabeledRotary *>> dial_groups = {
+      {&cacheSizeRotary},
+      {&grainSizeRotary, &grainShapeRotary, &grainRandomnessRotary,
+       &grainOverlapRotary},
+      {&wetDryRotary}};
+
+  EnvelopeGraphic grain_shape_graphic{getDialX(2) + ROTARY_WIDTH / 2, 75};
+
+  int getDialX(int dial_index) {
+    std::vector<int> group_sizes;
+    for (std::vector<LabeledRotary *> group : dial_groups) {
+      group_sizes.push_back(group.size());
     }
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (QuickSandAudioProcessorEditor)
+    int x_pos = 0, acc = 0;
+    for (int s : group_sizes) {
+      int group_end = acc + s;
+
+      bool dial_in_group = dial_index < group_end;
+      x_pos += ROTARY_WIDTH * (dial_in_group ? dial_index - acc : s);
+      if (dial_in_group)
+        break;
+
+      x_pos += GROUP_PAD;
+      acc += s;
+    }
+    return x_pos;
+  }
+
+  int getGroupX(int group_index) {
+    int dial_count = 0;
+    for (int g = 0; g < group_index; ++g) {
+      dial_count += dial_groups[g].size();
+    }
+    return getDialX(dial_count);
+  }
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(QuickSandAudioProcessorEditor)
 };
