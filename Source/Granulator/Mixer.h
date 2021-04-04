acc8e3452d620587a479dcbca0c726a5f8803149
Ciaran <ciaran.deg@gmail.com>
2021-04-03 18:36:40 -0600

    Add Mixer logic

--- Source/Granulator/Mixer.h
+++ Source/Granulator/Mixer.h
@@ -9,3 +9,23 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+class Mixer {
+public:
+  std::vector<float> mix(std::vector<float> dry, std::vector<float> wet,
+                         float pct_wet) {
+    // assert(dry.size() == wet.size());
+    auto size = dry.size();
+    float pct_dry = 1.f - pct_wet;
+    std::vector<float> out;
+
+    out.resize(size);
+    for (int samp = 0; samp < size; ++samp) {
+      out[samp] = (wet[samp] * pct_wet) + (dry[samp] * pct_dry);
+    }
+
+    return out;
+  }
+};
