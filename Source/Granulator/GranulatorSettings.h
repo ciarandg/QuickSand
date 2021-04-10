70b6df6b429a0d7a5b118fc65ebe1be8dc7b8358
Ciaran <ciaran.deg@gmail.com>
2021-04-10 12:58:05 -0600

    Add parameterized constructor to GranulatorSettings

--- Source/Granulator/GranulatorSettings.h
+++ Source/Granulator/GranulatorSettings.h
@@ -12,8 +12,12 @@
 
 class GranulatorSettings {
 public:
-  int grainSize = 20;
-  float grainShape = 0.5;
-  float randomness = 0.8;
-  float overlap = 1.0;
+  GranulatorSettings(int grainSize = 44100, float grainShape = 0.5,
+                     float randomness = 0.8, float overlap = 1.0)
+      : grainSize{grainSize}, grainShape{grainShape},
+        randomness{randomness}, overlap{overlap} {}
+  int grainSize;      // grain size (in samples)
+  float grainShape;   // [0.0, 1.0] interpolates from square to triangle
+  float randomness;   // [0.0, 1.0] how far back in the cache can we pull samples from?
+  float overlap;      // [0.5, 2.0] ranges from interlinked to equally spaced
 };
