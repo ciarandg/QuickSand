77c042c04be4f48c98920b2b6cf5d9095f061743
Ciaran <ciaran.deg@gmail.com>
2021-04-10 18:30:57 -0600

    Add test for minimum overlap

--- Source/Test/GranulatorTest.h
+++ Source/Test/GranulatorTest.h
@@ -40,13 +40,47 @@ public:
       expect(grain[s] == CACHE_SIZE - GRAIN_SIZE + (s % GRAIN_SIZE));
 
     beginTest("Test partial grain");
-    grain = gran.read(GRAIN_SIZE * 2.5);
-    expect(grain.size() == GRAIN_SIZE * 2.5);
+    int partial = GRAIN_SIZE * 2.5;
+    grain = gran.read(partial);
+    // grain: {35, 36, 37, 38, 39, 35, 36, 37, 38, 39, 35, 36}
+    expect(grain.size() == partial);
     for (int s = 0; s < grain.size(); ++s)
       expect(grain[s] == CACHE_SIZE - GRAIN_SIZE + (s % GRAIN_SIZE));
 
-    beginTest("Test overlap");
+    beginTest("Test overlap 0.5");
+    gran.clear_overhang();
+    int even_grain_size = GRAIN_SIZE % 2 == 0 ? GRAIN_SIZE : GRAIN_SIZE + 1;
+    settings.grainSize = even_grain_size;
+    settings.overlap = 0.5f;
+    partial = even_grain_size * 1.5;
+    grain = gran.read(partial);
+
+    // grain: 34, 35, 36, 37, 38, 39,
+    //        +           34, 35, 36, 37, 38, 39,
+    //        +                       34, 35, 36
+    //
+    //        = {34, 35, 36, 71, 73, 75, 71, 73, 75}
+
+    expect(grain.size() == partial);
+    for (int s = 0; s < grain.size(); ++s) {
+      int g1 = s < even_grain_size
+                   ? CACHE_SIZE - even_grain_size + (s % even_grain_size)
+                   : 0;
+      int g2 = s >= even_grain_size * 0.5
+                   ? CACHE_SIZE - even_grain_size +
+                         (int(s + 0.5 * even_grain_size) % even_grain_size)
+                   : 0;
+      int g3 = s >= even_grain_size
+                   ? CACHE_SIZE - even_grain_size + (s % even_grain_size)
+                   : 0;
+      expect(grain[s] == g1 + g2 + g3);
+    }
+
+    beginTest("Test overlap 2.0");
+    gran.clear_overhang();
+    settings.overlap = 2.0f;
     // ? what if grain size is larger than cache size
+    beginTest("Test clear_overhang()");
   }
 
 private:
