977a5696fd8e37997019544eac2adda227a522fc
Ciaran <ciaran.deg@gmail.com>
2021-04-11 11:15:13 -0600

    Register MultiGranulatorTest

--- Source/Test/TestSuite.h
+++ Source/Test/TestSuite.h
@@ -12,9 +12,11 @@
 
 #include "RollingCacheTest.h"
 #include "GranulatorTest.h"
+#include "MultiGranulatorTest.h"
 
 class TestSuite {
 private:
   const RollingCacheTest rolling_cache_test{};
   const GranulatorTest granulator_test{};
+  const MultiGranulatorTest multi_granulator_test{};
 };
